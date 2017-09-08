307ee8019f2e2375fc74a9d296a83e4a05c48eb1
Ashok <ashok.arulsamy@gmail.com>
2017-09-07 22:26:20 -0700

    Tally add data implemented

---
+++ src/main/java/com/india/tamilnadu/jaxrs/TallyServiceImpl.java
@@ -0,0 +1,18 @@
+package com.india.tamilnadu.jaxrs;
+
+import java.util.List;
+import com.india.tamilnadu.dao.ProductsDAO;
+import com.india.tamilnadu.dao.SuppliersDAO;
+import com.india.tamilnadu.dto.Response;
+
+public class TallyServiceImpl implements TallyService {
+	
+	public Response addTallyData(String tallyData) {
+		System.out.println("...adding tally data");
+				
+		System.out.println("Tally Data : " + tallyData);
+		
+		return new Response();
+	}
+	
+}
